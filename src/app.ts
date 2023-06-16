2bf386f0625941e15c289d7afd89c1feb7f77993
RUS29TAM <rustam.nikonchuk@yandex.ru>
2023-06-16 15:22:51 +0300

    chore: add router for user. Create request get/post

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,20 @@
 import express from "express";
 import mongoose from "mongoose";
+import usersRoute from "./routes/users-route";
 
 const app = express();
 const { PORT = 3000 } = process.env;
 
-app.get("/", (req, res) => {
-  res.send(`<h1 style="color: blueviolet; text-align: center">"HI RUS! Server start on PORT ${PORT}"</h1>`);
-});
+app.use(express.json());
+app.use("/users", usersRoute);
 
-app.listen(PORT, () => {
-  console.log(`Слушаем порт ${PORT}`);
-});
+// app.get("/", (req, res) => {
+//   res.send(`<h1 style="color: blueviolet; text-align: center">"HI RUS! Server start on PORT ${PORT}"</h1>`);
+// });
+
+app.listen(PORT);
+// app.listen(PORT, () => {
+//   console.log(`Слушаем порт ${PORT}`);
+// });
 
 mongoose.connect("mongodb://127.0.0.1:27017/mestodb");
