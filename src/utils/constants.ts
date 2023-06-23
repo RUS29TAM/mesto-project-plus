fb079677befafd2ad24f136753bd16111c971fb3
RUS29TAM <rustam.nikonchuk@yandex.ru>
2023-06-23 11:21:05 +0300

    chore: add constants

---
+++ src/utils/constants.ts
@@ -0,0 +1,9 @@
+// eslint-disable-next-line import/prefer-default-export
+export const defaultUser = {
+  name: "defaultName",
+  about: "defaultAbout",
+  avatar: "https://moysite.ru/wp-content/uploads/2023/02/jandeks.praktikum-logo.jpg"
+};
+
+export const urlRegExp: RegExp = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
+export const DB_URL = "mongodb://127.0.0.1:27017/mestodb";
