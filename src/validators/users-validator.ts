345ca1e1cbe1f76a6356cdb3f2208631a76a1658
RUS29TAM <rustam.nikonchuk@yandex.ru>
2023-06-23 11:19:32 +0300

    feat: user validation

--- src/validators/users-validator.ts
+++ src/validators/users-validator.ts
@@ -5,21 +5,53 @@ import { urlRegExp } from "../utils/utils";
 export const getUserByIdValidation = celebrate({
   [Segments.PARAMS]: Joi.object().keys({
     userId: Joi.string().length(24).hex().required()
+      .messages({
+        "string.base": "userId должен быть строкой",
+        "string.empty": "userId не должен быть пустым",
+        "string.length": "userId должен быть длиной {#limit} символов",
+        "string.hex": "userId должен быть шестнадцатеричным числом",
+        "string.required": "userId обязателен для заполнения",
+      })
   }),
 });
 
 export const updateCurrentUserDataValidation = celebrate({
   [Segments.BODY]: Joi.object().keys({
-    name: Joi.string().min(2).max(30).required(),
+    name: Joi.string().min(2).max(30).required()
+      .messages({
+        "string.empty": "Поле 'Имя' не должно быть пустым",
+        "string.min": "Поле 'Имя'  должно содержать не менее {#limit} символов",
+        "string.max": "Поле 'Имя'  должно содержать не более {#limit} символов",
+        "string.required": "Поле Имя обязателено для заполнения",
+      }),
     about: Joi.string().min(2).max(200).required()
+      .messages({
+        "string.empty": "Поле 'about' не должно быть пустым",
+        "string.min": "Поле 'about'  должно содержать не менее {#limit} символов",
+        "string.max": "Поле 'about'  должно содержать не более {#limit} символов",
+        "string.required": "Поле 'about' обязателено для заполнения",
+      }),
   }),
 });
 
 export const createUserValidation = celebrate({
   [Segments.BODY]: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(200),
-    avatar: Joi.string().pattern(urlRegExp)
+    name: Joi.string().min(2).max(30)
+      .messages({
+        "string.empty": "Поле 'Имя' не должно быть пустым",
+        "string.min": "Поле 'Имя'  должно содержать не менее {#limit} символов",
+        "string.max": "Поле 'Имя'  должно содержать не более {#limit} символов",
+      }),
+    about: Joi.string().min(2).max(200)
+      .messages({
+        "string.empty": "Поле 'about' не должно быть пустым",
+        "string.min": "Поле 'about'  должно содержать не менее {#limit} символов",
+        "string.max": "Поле 'about'  должно содержать не более {#limit} символов",
+      }),
+    avatar: Joi.string().pattern(urlRegExp).messages({
+      "string.empty": "Поле 'Сслыка на аватар' не должно быть пустым",
+      "string.pattern.base": "Поле 'Сслыка на аватар' должно быть валидным URL-адресом",
+    }),
   }),
 });
 
