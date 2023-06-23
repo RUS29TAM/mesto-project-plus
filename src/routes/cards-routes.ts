de91077afa930e9e93d1838898c2dd98e80ffded
RUS29TAM <rustam.nikonchuk@yandex.ru>
2023-06-23 11:23:53 +0300

    chore: add validation in card route

--- src/routes/cards-routes.ts
+++ src/routes/cards-routes.ts
@@ -1,11 +1,26 @@
 import { Router } from "express";
+import {
+  getCards,
+  likeCardById,
+  dislikeCardById,
+  deleteCardById,
+  createCard,
+} from "../controllers/cards-controller";
+import {
+  createCardValidation,
+  deleteCardByIdValidation,
+  dislikeCardByIdValidation,
+  likeCardByIdValidation
+} from "../validators/cards-validator";
 
 const router = Router();
 
 router.get("/", getCards);
-router.post("/", createCard);
+router.post("/", createCardValidation, createCard);
 
-router.delete("/:cardId", deleteCardById);
+router.delete("/:cardId", deleteCardByIdValidation, deleteCardById);
 
-router.put("/:cardId/likes", likeCardById);
-router.delete("/:cardId/likes", dislikeCardById);
+router.put("/:cardId/likes", likeCardByIdValidation, likeCardById);
+router.delete("/:cardId/likes", dislikeCardByIdValidation, dislikeCardById);
+
+export default router;
