1a483bfce17d78eb83523d902b226f5bcc81288c
RUS29TAM <rustam.nikonchuk@yandex.ru>
2023-06-23 16:00:12 +0300

    chore: create a root router

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,11 @@ import express from "express";
 import mongoose from "mongoose";
 import { errors } from "celebrate";
 
-import usersRoute from "./routes/users-routes";
-import cardsRoutes from "./routes/cards-routes";
 import authMiddleware from "./middlewares/auth-middleware";
-import NotFoundError from "./errors/not-found-error";
 import unexpectedErrMiddleware from "./middlewares/unexpected-err-middleware";
 // eslint-disable-next-line import/named
 import { DB_URL } from "./utils/constants";
+import router from "./routes/index";
 
 const app = express();
 const { PORT = 3000 } = process.env;
@@ -17,13 +15,7 @@ mongoose.connect(DB_URL);
 
 app.use(express.json());
 app.use(authMiddleware);
-app.use("/users", usersRoute);
-app.use("/cards", cardsRoutes);
-
-app.use((req, res, next) => {
-  next(new NotFoundError("Не существующая страница"));
-});
-
+app.use("/", router);
 app.use(errors());
 app.use(unexpectedErrMiddleware);
 
