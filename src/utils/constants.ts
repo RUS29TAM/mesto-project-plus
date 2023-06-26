0b60c925744f4e5ceccdc281b136a251ab5b9c21
Rustam Nikonchuk <rustam.nikonchuk@yandex.ru>
2023-06-26 15:43:31 +0300

    chore: add constants for validation email input

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -7,6 +7,7 @@ export const defaultUser = {
 };
 
 export const urlRegExp: RegExp = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)/;
+export const emailRegExp: RegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 export const DB_URL = 'mongodb://127.0.0.1:27017/mestodb';
 
 export const INTERNAL_SERVER_ERROR = http2.constants.HTTP_STATUS_INTERNAL_SERVER_ERROR;
