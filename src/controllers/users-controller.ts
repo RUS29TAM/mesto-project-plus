f51c998e86ec8736a5de324bc83f22351fabace7
Rustam Nikonchuk <rustam.nikonchuk@yandex.ru>
2023-06-27 17:40:14 +0300

    refactor: func createUser `add options email & password hashing method`

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,11 +1,13 @@
 import { NextFunction, Request, Response } from 'express';
 import { Error } from 'mongoose';
 
+import bcryptjs from 'bcryptjs';
 import userModel from '../models/user-model';
 import BadRequestError from '../errors/bad-request-error';
 import NotFoundError from '../errors/not-found-error';
 import { IHardcoreReq } from '../interfaces/i-hardcore-req';
-import { OK } from '../utils/constants';
+import { CREATED, DUPLICATE_KEY_ERROR } from '../utils/constants';
+import ConflictError from '../errors/conflict-error';
 
 export const getUsers = async (req: Request, res: Response, next: NextFunction) => {
   try {
@@ -35,19 +37,58 @@ export const getUserById = async (req: Request, res: Response, next: NextFunctio
 };
 
 export const createUser = async (req: Request, res: Response, next: NextFunction) => {
-  const { name, about, avatar } = req.body;
-
   try {
-    const user = await userModel.create({ name, about, avatar });
-    res.status(OK).send({ data: user });
-  } catch (err) {
+    const {
+      name, about, avatar, email, password,
+    } = req.body;
+
+    const hash = await bcryptjs.hash(password, 10);
+
+    const user = await userModel.create({
+      name,
+      about,
+      avatar,
+      password: hash,
+      email,
+    });
+
+    res.status(CREATED).send({
+      data: {
+        name: user.name,
+        about: user.about,
+        avatar: user.avatar,
+        email: user.email,
+        _id: user._id,
+      },
+    });
+  } catch (err: any) {
     if (err instanceof Error.ValidationError) {
       next(new BadRequestError(err.message));
+    } else if (err.code === DUPLICATE_KEY_ERROR) {
+      next(new ConflictError('Такой e-mail уже существует'));
     } else {
       next(err);
     }
   }
 };
+// export const createUser = async (req: Request, res: Response, next: NextFunction) => {
+//   const {
+//     name, about, avatar, email, password,
+//   } = req.body;
+
+//   try {
+//     const user = await userModel.create({
+//       name, about, avatar, email, password,
+//     });
+//     res.status(OK).send({ data: user });
+//   } catch (err) {
+//     if (err instanceof Error.ValidationError) {
+//       next(new BadRequestError(err.message));
+//     } else {
+//       next(err);
+//     }
+//   }
+// };
 
 const findUser = async (req: IHardcoreReq, res: Response, next: NextFunction) => {
   const userId = req.user?._id ?? req.params.userId;
