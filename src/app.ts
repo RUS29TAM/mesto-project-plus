1343f11439b5ddb99dd2e057c54ccd14a654d5c0
Rustam Nikonchuk <rustam.nikonchuk@yandex.ru>
2023-07-03 12:34:54 +0300

    feat: `add restrictions on repeated API requests when logging in to your account`.

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import { errors } from 'celebrate';
 import cors from 'cors';
 import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
 
 import authMiddleware from './middlewares/auth-middleware';
 import unexpectedErrMiddleware from './middlewares/unexpected-err-middleware';
@@ -11,7 +12,10 @@ import { DB_URL } from './utils/constants';
 import router from './routes/index';
 import { errorLogger, setupLogger } from './middlewares/logger-middleware';
 import { createUserValidation, loginValidation } from './validators/users-validator';
+
 import { createUser, login } from './controllers/users-controller';
+
+const limiter = rateLimit({ windowMs: 15 * 60 * 1000, max: 5, message: 'Повторите попытку через 15 мин.' });
 
 const createServer = async () => {
   try {
@@ -28,7 +32,7 @@ const createServer = async () => {
   app.use(helmet());
   app.use(express.json());
   app.use(setupLogger);
-  app.use('/signin', loginValidation, login);
+  app.use('/signin', limiter, loginValidation, login);
   app.use('/signup', createUserValidation, createUser);
   app.use(authMiddleware);
   app.use('/', router);
